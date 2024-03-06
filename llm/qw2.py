51c5b1024ec4fd2e3de49a5841019628834dc3cf
gt <jack139@gmail.com>
2024-03-06 18:00:40 +0800

    qwen2-0.5b测试

--- llm/qw2.py
+++ llm/qw2.py
@@ -4,63 +4,67 @@ import readline
 import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer, GenerationConfig
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--model_path', default=".", type=str)
-parser.add_argument('--max_length', default=200, type=int)
-parser.add_argument('--no_compile', action='store_true', help='do not compile model (PyTorch < 2.0)')
-args = parser.parse_args()
 
-if torch.cuda.is_available():
-    torch.set_default_device("cuda")
-    load_in_4bit = True
-else:
-    torch.set_default_device("cpu")
-    load_in_4bit = False
-
-print(f"load_in_4bit = {load_in_4bit}")
+model_path = "../lm_model"
+device = "cuda"
 
 start = time.time()
-print("Load model QWen-1.8B-Chat ...")
+
+print("Load model Qwen1.5-0.5B-Chat ...")
 model = AutoModelForCausalLM.from_pretrained(
-    f"{args.model_path}/Qwen-1_8B-Chat", 
-    load_in_8bit=False,
-    low_cpu_mem_usage=True,
+    f"{model_path}/Qwen1.5-0.5B-Chat", 
+    #load_in_8bit=False,
+    #load_in_4bit=False,
     device_map='auto',
-    load_in_4bit=load_in_4bit,
-    trust_remote_code=True,
+    torch_dtype="auto",
 )
-model.generation_config = GenerationConfig.from_pretrained(
-    f"{args.model_path}/Qwen-1_8B-Chat", trust_remote_code=True)
+#model.generation_config = GenerationConfig.from_pretrained(
+#    f"{model_path}/Qwen1.5-0.5B-Chat", trust_remote_code=True)
 
-tokenizer = AutoTokenizer.from_pretrained(
-    f"{args.model_path}/Qwen-1_8B-Chat", trust_remote_code=True)
+tokenizer = AutoTokenizer.from_pretrained(f"{model_path}/Qwen1.5-0.5B-Chat")
 
-if not args.no_compile:
-    print("compiling the model... (takes a ~minute)")
-    model = torch.compile(model) # requires PyTorch 2.0 (optional)
+print("compiling the model... (takes a ~minute)")
+model = torch.compile(model) # requires PyTorch 2.0 (optional)
+
 print(f"Time elapsed: {(time.time() - start):.3f} sec.")
 
 
 with torch.no_grad():
     print("Start inference mode.")
     print('=' * 85)
-    history = None
 
     while True:
-        raw_input_text = input("(## to clear history; !! to quit.) Prompt:")
+        raw_input_text = input("(!! to quit.) 请输入您的问题：")
         raw_input_text = str(raw_input_text)
-        if raw_input_text.strip() == "##":
-            history = None
-            print("History cleared.")
-            continue
         if raw_input_text.strip() == "!!":
             print("Bye!")
             break
         if len(raw_input_text.strip()) == 0:
             continue
 
+        messages = [
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": raw_input_text}
+        ]
+
         start = time.time()
-        response, history = model.chat(tokenizer, raw_input_text, history=history)
+
+        text = tokenizer.apply_chat_template(
+            messages,
+            tokenize=False,
+            add_generation_prompt=True
+        )
+        model_inputs = tokenizer([text], return_tensors="pt").to(device)
+
+        generated_ids = model.generate(
+            model_inputs.input_ids,
+            max_new_tokens=512
+        )
+        generated_ids = [
+            output_ids[len(input_ids):] for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)
+        ]
+
+        response = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
+
         print("Response: ", response)
-        print("\nHistory: ", history)
         print(f">>>>>>> Time elapsed: {(time.time() - start):.3f} sec.\n\n")
